7142d9ac71dd784801609d55530348db68901bfc
Madhu <142387122+madhudottota@users.noreply.github.com>
2024-06-16 12:48:26 +0530

    Update NetworkConnectivityObserverImpl.kt

--- app/src/main/java/com/example/imagevista/data/repository/NetworkConnectivityObserverImpl.kt
+++ app/src/main/java/com/example/imagevista/data/repository/NetworkConnectivityObserverImpl.kt
@@ -11,50 +11,56 @@ import com.example.imagevista.domain.repository.NetworkConnectivityObserver
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 
 class NetworkConnectivityObserverImpl(
-    context: Context,
-    scope: CoroutineScope
-): NetworkConnectivityObserver {
+    context: Context, scope: CoroutineScope
+) : NetworkConnectivityObserver {
 
     private val connectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-    private val _networkStatus = observe().stateIn(
-        scope = scope,
-        started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
-        initialValue = NetworkStatus.Disconnected
-    )
-    override val networkStatus: StateFlow<NetworkStatus> = _networkStatus
+    private val _networkStatus = MutableStateFlow(NetworkStatus.Disconnected)
+    override val networkStatus: StateFlow<NetworkStatus> get() = _networkStatus
 
-    private fun observe(): Flow<NetworkStatus> {
+    init {
+        observeNetworkStatus()
+            .distinctUntilChanged()
+            .onEach { status ->
+                _networkStatus.value = status
+            }
+            .launchIn(scope)
+    }
+
+    private fun observeNetworkStatus(): Flow<NetworkStatus> {
         return callbackFlow {
             val connectivityCallback = object : NetworkCallback() {
                 override fun onAvailable(network: Network) {
-                    super.onAvailable(network)
-                    trySend(NetworkStatus.Connected)
+                    trySend(NetworkStatus.Connected).isSuccess
                 }
 
                 override fun onLost(network: Network) {
-                    super.onLost(network)
-                    trySend(NetworkStatus.Disconnected)
+                    trySend(NetworkStatus.Disconnected).isSuccess
                 }
             }
-            val request = NetworkRequest.Builder()
-                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-                .build()
+
+            val request =
+                NetworkRequest.Builder().addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                    .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR).build()
+
             connectivityManager.registerNetworkCallback(request, connectivityCallback)
+
             awaitClose {
                 connectivityManager.unregisterNetworkCallback(connectivityCallback)
             }
         }
-            .distinctUntilChanged()
     }
 }
